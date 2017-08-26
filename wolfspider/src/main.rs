d279149ece6d5eb6013bd8ee3b4d8220c468c626
Max Hunt <max@huntw3.com>
2017-08-26 04:13:48 -0700

    Getting closer to working.

--- wolfspider/src/main.rs
+++ wolfspider/src/main.rs
@@ -6,7 +6,65 @@ use std::fs::File;
 use std::io;
 use std::io::BufReader;
 use std::io::BufRead;
+use std::io::Read;
+use std::io::Write;
 use regex::Regex;
+use std::collections::HashMap;
+
+struct Chunk {
+    tag: String,
+    content: String,
+}
+
+struct WFSFileCache {
+    file_chunks: HashMap<String, Vec<Chunk>>,
+}
+
+impl WFSFileCache {
+    fn new() -> WFSFileCache {
+        WFSFileCache{file_chunks: HashMap::new()}
+    }
+
+    fn load(&mut self, filename: &str) -> &Vec<Chunk> {
+        let mut file = File::open(filename).unwrap();
+        let mut content = String::new();
+        file.read_to_string(&mut content);
+        let re = Regex::new(r"\[([^\[\]]+)\].*").unwrap();
+        let mut chunks = Vec::new();
+        for cap in re.captures_iter(content.as_ref()) {
+            chunks.push(Chunk{tag: cap[1].to_string(), content: cap[2].to_string()});
+        }
+        self.file_chunks.insert(filename.to_string(), chunks);
+        self.file_chunks.get(filename).unwrap()
+    }
+
+    fn get(&mut self, filename: &str) -> String {
+        let chunks = if self.file_chunks.contains_key(filename) {
+            self.file_chunks.get(filename).unwrap()
+        } else  {
+            self.load(filename)
+        };
+        let mut whole_file = String::new();
+        for ch in chunks {
+            whole_file.push_str(&ch.content);
+        }
+        whole_file
+    }
+
+    fn get_tagged(&mut self, filename: &str, tag: &str) -> String {
+        let chunks = if self.file_chunks.contains_key(filename) {
+            self.file_chunks.get(filename).unwrap()
+        } else  {
+            self.load(filename)
+        };
+        for ch in chunks {
+            if ch.tag == tag {
+                return ch.content;
+            }
+        }
+        panic!("Couldn't find tag.")
+    }
+}
 
 // wolfspidertag build-order-struct
 enum Action {
@@ -32,10 +90,11 @@ impl From<io::Error> for BuildError {
 impl BuildOrder {
     fn build(&self, outfile: &str) -> Result<(), BuildError> {
         let outfile = File::create(outfile)?;
+        let mut read_files = WFSFileCache::new();
         for action in &self.actions {
             match action {
-                &Action::WholeFile { ref file } => println!("{}", file),
-                &Action::Tag { ref file, ref tag } => println!("{} tag {}", file, tag),
+                &Action::WholeFile { ref file } => outfile.write(read_files.get(file).as_bytes())?,
+                &Action::Tag { ref file, ref tag } => outfile.write(read_files.get_tagged(file, tag).as_bytes())?,
             }
         }
         Ok(())
